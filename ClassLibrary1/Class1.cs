1b5a4f8d9f6784e1b67dfb92f8a674a68c16ecd4
simon <simonkq1@gmail.com>
2018-10-08 15:16:23 +0800

    C# Learning

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
